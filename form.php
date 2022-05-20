0e5204bc91fe4bd77747d9b88a0f035f7a237f60
EHinrich <90458838+EHinrich@users.noreply.github.com>
2022-05-21 00:16:08 +0300

    Update form.php

--- form.php
+++ form.php
@@ -130,7 +130,7 @@ if (!empty($messages)) {
                          <?php if($values['check']==TRUE){print 'checked';} ?> />
                   С контрактом ознакомлен
               </label><br />
-                 <input   name="csrf" value="<?php print $token; ?>">
+                 <input tupy="hidden" name="csrf" value="<?php print $token; ?>">
       
               Отправить данные:
               <input type="submit" value="Отправить" />
